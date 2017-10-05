1968b18ffc3d75962a6f89e7dcc878a75f7dd5a9
carlillostole <carliyostole@gmail.com>
2017-10-05 12:21:19 +0200

    funciones_bd y test_bd actualizados

---
+++ TheWeatherBOT/funciones_bd.py
@@ -0,0 +1,47 @@
+#!/bin/usr/python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+def ciudades(consulta):
+    db = sqlite3.connect('datosciudad.db') # conectamos con la bd
+    b = db.cursor()
+    b.execute(consulta) # Obtenemos el resultado de la consulta
+    resp = ""
+    for r in b:
+        # Mandamos un string con un formato válido.
+        aux = str(r)
+        pos1 = aux.find("'")
+        pos2 = aux.find("'", len(aux)-3)
+        final = ""
+        for i in range(pos1+1, pos2):
+            final += aux[i]
+        resp += str(final) + "\n"
+
+    # Cierre de conexión con la base de datos.
+    b.close()
+    db.close()
+    return resp
+
+def cuentaCiudades():
+    """ Función que devuelve el total de actividades disponibles """
+    # Conectamos con la base de datos
+    db = sqlite3.connect('datosciudad.db')
+    b = db.cursor()
+    b.execute("SELECT * FROM ciudades") # Obtenemos el resultado de la consulta
+    total = 0
+    # Obtenemos la cantidad de ciudades disponibles.
+    for i in b:
+        total += 1
+    # Cerramos la base de datos
+    b.close()
+    db.close()
+    return total
+
+def insertar(ciudad, temperatura, humedad,presion):
+    db = sqlite3.connect('datosciudad.db')
+    b = db.cursor()
+    b.execute("INSERT INTO ciudades (ciudad, temperatura, humedad,presion) VALUES (?, ?, ?,?)", (ciudad, temperatura, humedad,presion))
+    db.commit()
+    b.close()
+	#db.close()
---
+++ TheWeatherBOT/test_bd.py
@@ -0,0 +1,27 @@
+#!/bin/usr/python
+# -*- coding: utf-8 -*-
+
+import unittest
+import funciones_bd
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_existe_ciudades(self):
+        """ Test que comprueba si hay ciudades disponibles. """
+        total = funciones_bd.cuentaCiudades()
+        self.assertNotEqual(total, 0)
+
+    def test_nombre_ciudad(self):
+        """ Test que devuelve true si hay una ciudad con una temperatura concreta. """
+        ciudad_disponible = funciones_bd.ciudades("SELECT * FROM ciudades WHERE temperatura='10'")
+        self.assertIsNotNone(ciudad_disponible)
+
+    def test_temperatura_ciudad(self):
+        """ Test que devuelve true si el nombre de la ciudad coincide"""
+        nombre = funciones_bd.ciudades("SELECT ciudad FROM ciudades WHERE ciudad='granada'")
+        self.assertEqual(nombre, "granada\n")
+
+
+
+if __name__ == '__main__':
+	unittest.main()
