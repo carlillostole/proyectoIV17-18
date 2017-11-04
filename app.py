ccb4ac87893856fe317399da43f8f4676d533284
carlillostole <carliyostole@gmail.com>
2017-11-04 13:20:38 +0100

    app.py actualizada

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask
 from datetime import datetime
+import json
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-
-    return """
-    <h1>Heroku desplegado correctamente</h1>
-    <h2>Status heroku: OK</h2>
-    <a href="tg://resolve?domain=carlillostole_weatherBot">Acceso al bot de telegram</a>
-
-    <p>It is currently {time}.</p>
-    """.format(time=the_time)
+def status():
+    data={"status":"OK"}
+    return json.dumps(data) 
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
