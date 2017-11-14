f145d4ded7e4677d1245f29f40492f60db3d49e2
carlillostole <carliyostole@gmail.com>
2017-11-14 19:00:14 +0100

    haciendo cambios app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 import json
 
@@ -10,8 +10,12 @@ def status():
     data={"status":"OK"}
     return json.dumps(data) 
 
+
 @app.route('/')
-return
+def index():
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
