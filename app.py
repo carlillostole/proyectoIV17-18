c527ca4b496b9e06fac1779254214e139cb3c05f
carlillostole <carliyostole@gmail.com>
2017-11-21 19:32:17 +0100

    error /

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from datetime import datetime
+from flask import Flask,request, jsonify
+import os
 import json
 
 app = Flask(__name__)
@@ -9,10 +9,11 @@ app = Flask(__name__)
    "status": "OK"
 }
 
+
+
 @app.route("/")
-def status():
+def principal():
     return jsonify(status='OK')
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
+if __name__ == "__main__":
+        app.run(debug = True, use_reloader = True)
