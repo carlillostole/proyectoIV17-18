14583f9a4f849a577b719cddf32c4c6e59873bc3
carlillostole <carliyostole@gmail.com>
2017-11-21 19:20:33 +0100

    Error /

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ app = Flask(__name__)
 def rutaStatus():
     return jsonify(status='OK')
 
+#@app.route("/status")
+#def rutaStatus():
+#    return jsonify(status='OK')
+
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
