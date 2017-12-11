7583ca68948184cfc5fbdea7e08bc0b2988aba40
carlillostole <carliyostole@gmail.com>
2017-12-11 18:34:15 +0100

    Configuracion fabric close #29

---
+++ fabfile.py
@@ -0,0 +1,30 @@
+from fabric.api import *
+import os
+
+def descargar():
+    run ('sudo rm -rf proyectoIV17-18')
+    run ('sudo git clone https://github.com/carlillostole/proyectoIV17-18')
+
+def detener():
+    run ("sudo supervisorctl stop theweather")
+
+def borrar():
+    run ('sudo rm -rf proyectoIV17-18')
+
+def instalar():
+    run ('cd proyectoIV17-18 && make install')
+
+def recargar():
+    run("sudo supervisorctl reload")
+
+def iniciar():
+    with shell_env(TOKENBOT="454323731:AAHV_dXizf08VAkEzfMUgOKN9VaM5KCFExI"):
+        run('sudo supervisorctl start theweather')
+
+def iniciar_no_supervisor():
+    with shell_env(TOKENBOT="454323731:AAHV_dXizf08VAkEzfMUgOKN9VaM5KCFExI"):
+        run('cd proyectoIV17-18 && make execute')
+	
+def iniciar_hup():
+    with shell_env(TOKENBOT="454323731:AAHV_dXizf08VAkEzfMUgOKN9VaM5KCFExI"):
+        run ('nohup python proyectoIV17-18/TheWeatherBOT/bot.py >& /dev/null &',pty=False)
